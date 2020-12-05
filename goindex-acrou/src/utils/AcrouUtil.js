612fd54824a5c69bb3b78a2ffade022cfa4987ad
Dan <2660574+shirtjs@users.noreply.github.com>
2020-12-04 20:40:52 -0500

    release
    yes

--- goindex-acrou/src/utils/AcrouUtil.js
+++ goindex-acrou/src/utils/AcrouUtil.js
@@ -111,45 +111,6 @@ export function get_filex(path, callback) {
   });
 }
 
-//时间转换
-export function utc2beijing(utc_datetime) {
-  // 转为正常的时间格式 年-月-日 时:分:秒
-  var T_pos = utc_datetime.indexOf("T");
-  var Z_pos = utc_datetime.indexOf("Z");
-  var year_month_day = utc_datetime.substr(0, T_pos);
-  var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1);
-  var new_datetime = year_month_day + " " + hour_minute_second; // 2017-03-31 08:02:06
-
-  // 处理成为时间戳
-  var timestamp = new Date(Date.parse(new_datetime));
-  timestamp = timestamp.getTime();
-  timestamp = timestamp / 1000;
-
-  // 增加8个小时，北京时间比utc时间多八个时区
-  var unixtimestamp = timestamp + 8 * 60 * 60;
-
-  // 时间戳转为时间
-  unixtimestamp = new Date(unixtimestamp * 1000);
-  var year = 1900 + unixtimestamp.getYear();
-  var month = "0" + (unixtimestamp.getMonth() + 1);
-  var date = "0" + unixtimestamp.getDate();
-  var hour = "0" + unixtimestamp.getHours();
-  var minute = "0" + unixtimestamp.getMinutes();
-  var second = "0" + unixtimestamp.getSeconds();
-  return (
-    year +
-    "-" +
-    month.substring(month.length - 2, month.length) +
-    "-" +
-    date.substring(date.length - 2, date.length) +
-    " " +
-    hour.substring(hour.length - 2, hour.length) +
-    ":" +
-    minute.substring(minute.length - 2, minute.length) +
-    ":" +
-    second.substring(second.length - 2, second.length)
-  );
-}
 
 export function formatFileSize(bytes) {
   if (bytes >= 1000000000) {
--- themes/material/app2.js
+++ themes/material/app2.js
@@ -334,7 +334,7 @@ function append_files_to_list(path, files) {
       item['size'] = "";
     }
 
-    item['modifiedTime'] = utc2beijing(item['modifiedTime']);
+    item['modifiedTime'] = item['modifiedTime'];
     item['size'] = formatFileSize(item['size']);
     if (item['mimeType'] == 'application/vnd.google-apps.folder') {
       html += `<li class="mdui-list-item mdui-ripple"><a href="${p}" class="folder">
@@ -552,7 +552,7 @@ function append_search_result_to_list(files) {
       item['size'] = "";
     }
 
-    item['modifiedTime'] = utc2beijing(item['modifiedTime']);
+    item['modifiedTime'] = item['modifiedTime'];
     item['size'] = formatFileSize(item['size']);
     if (item['mimeType'] == 'application/vnd.google-apps.folder') {
       html += `<li class="mdui-list-item mdui-ripple"><a id="${item['id']}" onclick="onSearchResultItemClick(this)" class="folder">
@@ -934,36 +934,7 @@ function file_image(path) {
 }
 
 
-//时间转换
-function utc2beijing(utc_datetime) {
-  // 转为正常的时间格式 年-月-日 时:分:秒
-  var T_pos = utc_datetime.indexOf('T');
-  var Z_pos = utc_datetime.indexOf('Z');
-  var year_month_day = utc_datetime.substr(0, T_pos);
-  var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1);
-  var new_datetime = year_month_day + " " + hour_minute_second; // 2017-03-31 08:02:06
 
-  // 处理成为时间戳
-  timestamp = new Date(Date.parse(new_datetime));
-  timestamp = timestamp.getTime();
-  timestamp = timestamp / 1000;
-
-  // 增加8个小时，北京时间比utc时间多八个时区
-  var unixtimestamp = timestamp + 8 * 60 * 60;
-
-  // 时间戳转为时间
-  var unixtimestamp = new Date(unixtimestamp * 1000);
-  var year = 1900 + unixtimestamp.getYear();
-  var month = "0" + (unixtimestamp.getMonth() + 1);
-  var date = "0" + unixtimestamp.getDate();
-  var hour = "0" + unixtimestamp.getHours();
-  var minute = "0" + unixtimestamp.getMinutes();
-  var second = "0" + unixtimestamp.getSeconds();
-  return year + "-" + month.substring(month.length - 2, month.length) + "-" + date.substring(date.length - 2, date.length)
-    + " " + hour.substring(hour.length - 2, hour.length) + ":"
-    + minute.substring(minute.length - 2, minute.length) + ":"
-    + second.substring(second.length - 2, second.length);
-}
 
 // bytes自适应转换到KB,MB,GB
 function formatFileSize(bytes) {
